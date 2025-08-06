47291356dd1989dc4a06fc9d7e4843faf441d0ba
Jannatul Ferdausy Keya <jfkeya21@gmail.com>
2025-08-06 17:57:34 +0600

    Add files via upload

---
+++ class10.java
@@ -0,0 +1,6 @@
+public class class10{
+  public static void main(String[] args) {
+    String name = "John";
+    System.out.println(name);
+  }
+}
---
+++ class11.java
@@ -0,0 +1,6 @@
+public class class11 {
+  public static void main(String[] args) {
+    int myNum = 15;
+    System.out.println(myNum);
+  }
+}
